2726d1aa6566847f004fbb15f75dfc3d4272a6ac
Sha Kong-Brooks <shianra.story@gmail.com>
2022-01-29 01:57:13 +1100

    Enabled the function page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -59,14 +59,18 @@ export default function Navbar() {
                       </div>
                     </div>
                   </Link>
-                  <div className="border-transparent text-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                  <Link
+                    activeClassName="border-red-600 text-gray-900"
+                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    to="/categories/function"
+                  >
                     <div>
                       <div>Function</div>
-                      <div className="ml-0 lg:ml-6 text-xs text-gray-200">
+                      <div className="ml-0 lg:ml-6 text-xs text-gray-400">
                         Web apps and games
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
@@ -105,6 +109,13 @@ export default function Navbar() {
                 to="/categories/imagination"
               >
                 Imagination
+              </Link>
+              <Link
+                activeClassName="border-red-600 text-gray-900"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-70 items-center block pl-3 pr-4 py-2 border-l-4 font-medium"
+                to="/categories/function"
+              >
+                Function
               </Link>
             </div>
           </Disclosure.Panel>
