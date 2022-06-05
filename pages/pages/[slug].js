574e257106ccc50d690dbeb98bd624ac1338936e
Sha Kong-Brooks <shianra.story@gmail.com>
2022-06-05 14:49:03 +1000

    Some refactoring things

---
+++ pages/pages/[slug].js
@@ -0,0 +1,61 @@
+import ErrorPage from "next/error";
+import Head from "next/head";
+import { getAllPagesWithSlug, getPage } from "lib/graphcms";
+import { useRouter } from "next/router";
+
+import Container from "components/container";
+import PostBody from "components/pages/post-body";
+import PostHeader from "components/pages/post-header";
+import PostTitle from "components/pages/post-title";
+import { Header } from "components/layout";
+import { Layout } from "components/layout";
+
+export default function Page({ page, preview }) {
+  const router = useRouter();
+
+  if (!router.isFallback && !page?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
+              <Head>
+                <title>{page.title} | AI Dreams</title>
+                <meta property="og:image" content={page.ogImage.url} />
+              </Head>
+              <PostHeader title={page.title} />
+              <PostBody content={page.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getPage(params.slug, preview);
+  return {
+    props: {
+      preview,
+      page: data.page,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const pages = await getAllPagesWithSlug();
+  return {
+    paths: pages.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: true,
+  };
+}
