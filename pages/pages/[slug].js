e403ea69c9b995404ea601e1bf44ea6b73558c1a
Sha Kong-Brooks <shianra.story@gmail.com>
2022-06-10 12:57:57 +1000

    Reorganised page imports

--- pages/pages/[slug].js
+++ pages/pages/[slug].js
@@ -1,7 +1,8 @@
 import ErrorPage from "next/error";
 import Head from "next/head";
-import { getAllPagesWithSlug, getPage } from "lib/graphcms";
 import { useRouter } from "next/router";
+
+import { getAllPagesWithSlug, getPage } from "lib/graphcms";
 
 import Container from "components/container";
 import PostBody from "components/pages/post-body";
