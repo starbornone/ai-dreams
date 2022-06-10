f693db00c48aad04b4f700a740fe76155fc03f91
Sha Kong-Brooks <shianra.story@gmail.com>
2022-06-10 15:02:59 +1000

    Removed target blank from About link

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -8,11 +8,7 @@ export function Footer() {
       <Container>
         <div className="py-28 md:px-4 lg:px-0 grid grid-cols-1 md:grid-cols-3">
           <div className="mb-10 md:mb-0">
-            <Link href="https://aidreams.world/pages/about">
-              <a target="_blank" rel="noreferrer">
-                About
-              </a>
-            </Link>
+            <Link href="https://aidreams.world/pages/about">About</Link>
           </div>
           <div className="md:text-center mb-10 md:mb-0">
             Existing since 2021.
