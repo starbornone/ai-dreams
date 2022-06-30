b681ccc92652ed322f7369017dd23a62452df5dc
Sha Kong-Brooks <shianra.story@gmail.com>
2022-07-01 05:10:09 +1000

    Updated post title leading

--- components/pages/post-title.js
+++ components/pages/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-3xl md:text-4xl tracking-tighter leading-tight md:leading-none mb-12 text-center max-w-4xl mx-auto">
+    <h1 className="text-3xl md:text-4xl tracking-tighter mb-12 text-center max-w-4xl mx-auto">
       {children}
     </h1>
   );
