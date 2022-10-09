7781274077c7c96725015811d0cb9f28e3347311
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2022-10-09 22:02:51 +1100

    Improved spacing

--- components/layout/header.js
+++ components/layout/header.js
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <section className="flex items-center justify-between my-16">
       <div className="site-title">
-        <h1 className="text-4xl md:text-6xl tracking-tighter leading-tight md:pr-8">
+        <h1 className="text-5xl md:text-6xl tracking-tighter leading-tight md:pr-8 -mb-2 md:mb-0">
           <Link href="/">AI Dreams</Link>
         </h1>
         <div className="text-xl md:text-2xl text-left ml-24">
