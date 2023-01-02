215eb0857304a025b05e2129f31b728e7d08d971
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2023-01-03 03:31:13 +1100

    Changed the alignment of the "MORE POSTS" heading and made it an H2

--- components/more-posts/more-posts.js
+++ components/more-posts/more-posts.js
@@ -3,9 +3,9 @@ import { PostLink } from 'components'
 export function MorePosts({ posts }) {
     return (
         <div className="my-16 md:my-32">
-            <div className="site-title mb-8 text-4xl leading-tight tracking-tighter md:text-5xl">
+            <h2 className="text-center sm:text-left sm:ml-3 site-title mb-8 text-4xl leading-tight tracking-tighter md:text-5xl">
                 More Posts
-            </div>
+            </h2>
 
             {posts.length > 0
                 ? posts.map((post) => <PostLink key={post.slug} post={post} />)
