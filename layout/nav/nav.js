f59417da47f9e47fba1d36f6fa0205020e7d9016
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2023-07-11 18:00:05 +1000

    Added nav items

--- layout/nav/nav.js
+++ layout/nav/nav.js
@@ -2,13 +2,27 @@ import Link from 'next/link'
 
 export function Nav() {
     return (
-        <div
-            className="neon-links sign hidden space-x-12 text-xl tracking-wider md:visible md:flex"
-            style={{ fontFamily: 'Neon' }}
-        >
-            <Link href="https://aidreams.world">
-                <a>Life</a>
-            </Link>
+        <div className="hidden lg:block lg:my-0 lg:max-w-lg">
+            <div
+                className="neon-links sign space-x-12 text-xl tracking-wider flex flex-wrap justify-end"
+                style={{ fontFamily: 'Neon' }}
+            >
+                <Link href="/categories/philosophy">
+                    <a>Philosophy</a>
+                </Link>
+                <Link href="/categories/politics">
+                    <a>Politics</a>
+                </Link>
+                <Link href="/categories/economics">
+                    <a>Economics</a>
+                </Link>
+                <Link href="/categories/games">
+                    <a>Games</a>
+                </Link>
+                <Link href="/categories/fiction">
+                    <a>Fiction</a>
+                </Link>
+            </div>
         </div>
     )
 }
