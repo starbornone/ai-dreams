172c56fde9cf293eb14baf7c610afcbf0bb6ef58
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2023-08-15 20:26:52 +1000

    Added culture and removed fiction from nav

--- layout/nav/nav.js
+++ layout/nav/nav.js
@@ -16,11 +16,11 @@ export function Nav() {
                 <Link href="/categories/economics">
                     <a>Economics</a>
                 </Link>
+                <Link href="/categories/culture">
+                    <a>Culture</a>
+                </Link>
                 <Link href="/categories/games">
                     <a>Games</a>
-                </Link>
-                <Link href="/categories/fiction">
-                    <a>Fiction</a>
                 </Link>
             </div>
         </div>
