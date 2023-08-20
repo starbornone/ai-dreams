0974cf99b72f17d05a04bba275e82907fdc957ab
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2023-08-21 01:42:13 +1000

    Updated tags and date section in Header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -18,16 +18,16 @@ export function Header({ coverImage, date, tags, title }: Props) {
         </div>
       )}
       {(tags || date) && (
-        <div className="flex justify-between max-w-3xl mx-auto mb-6">
+        <div className="flex items-center justify-between max-w-3xl mx-auto mb-6">
           {tags && (
-            <div className="flex gap-8 font-light text-text-500">
+            <div className="font-light text-gray-500 md:gap-8 md:flex">
               {tags.map((tag) => (
                 <div key={tag}>{tag}</div>
               ))}
             </div>
           )}
           {date && (
-            <div className="font-light text-text-500">
+            <div className="font-light text-gray-500">
               {format(new Date(date), 'dd MMMM yyyy')}
             </div>
           )}
