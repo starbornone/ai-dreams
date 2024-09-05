63606cfd9b4e0bb9d801354448142f73b4aa4e01
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-06 06:24:37 +1000

    Updated next config patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media.graphassets.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.graphassets.com',
+      },
+    ],
   },
 };
 
