e655d4687ee8625500440e3579ca79f9147c918a
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-12 02:05:18 +1000

    Shrunk the font

--- src/layout/nav/Nav.tsx
+++ src/layout/nav/Nav.tsx
@@ -5,7 +5,7 @@ export function Nav() {
   return (
     <div className="hidden lg:my-0 lg:block lg:max-w-lg">
       <div
-        className="neon-links sign flex flex-wrap justify-end space-x-12 text-xl tracking-wider"
+        className="neon-links sign flex flex-wrap justify-end space-x-12 text-lg tracking-wider"
         style={{ fontFamily: 'Neon' }}
       >
         {navList?.map((link) => (
