746177e3c77fcae027afddfc77d1d374f1f09ed1
Sha <sha.kongbrooks@gmail.com>
2024-09-25 02:56:35 +1000

    Converted Body to CSS

--- src/features/body/Body.tsx
+++ src/features/body/Body.tsx
@@ -3,9 +3,9 @@
 import hljs from 'highlight.js/lib/core';
 import typescript from 'highlight.js/lib/languages/typescript';
 import React from 'react';
+import styles from './Body.module.css';
 
 import 'highlight.js/styles/default.css';
-
 import 'highlight.js/styles/atom-one-dark.css';
 
 hljs.registerLanguage('typescript', typescript);
@@ -19,7 +19,7 @@ interface BodyProps {
 const addClassesToCodeTags = (html: string) => {
   return html.replace(/<code>(.*?)<\/code>/g, (match, code) => {
     const language = determineLanguage(code);
-    return `<code class="language-${language}">${code}</code>`;
+    return `<code class="${styles['body__code']} language-${language}">${code}</code>`;
   });
 };
 
@@ -37,5 +37,5 @@ export function Body({ content }: BodyProps) {
     hljs.highlightAll();
   }, [processedHtml]);
 
-  return <div dangerouslySetInnerHTML={{ __html: processedHtml }} />;
+  return <div className={styles['body']} dangerouslySetInnerHTML={{ __html: processedHtml }} />;
 }
