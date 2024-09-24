28eb00ae93cda416ef5568bb3befd7d730d9bc8e
Sha <sha.kongbrooks@gmail.com>
2024-09-25 02:58:39 +1000

    Converted Header to CSS

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,5 +1,6 @@
 import { CoverImage, Title } from '@/features';
 import { format } from 'date-fns';
+import styles from './Header.module.css';
 
 interface HeaderProps {
   coverImage?: { url: string };
@@ -12,20 +13,20 @@ export function Header({ coverImage, date, tags, title }: HeaderProps) {
   return (
     <>
       {coverImage && (
-        <div className="-mx-5 mb-8 sm:mx-0 md:mb-12">
+        <div className={styles['header']}>
           <CoverImage title={title} url={coverImage.url} />
         </div>
       )}
       {(tags || date) && (
-        <div className="mx-auto mb-6 flex max-w-3xl items-center justify-between">
+        <div className={styles['header__info']}>
           {tags && (
-            <div className="font-light text-gray-500 md:flex md:gap-8">
+            <div className={styles['header__tags']}>
               {tags.map((tag) => (
                 <div key={tag}>{tag}</div>
               ))}
             </div>
           )}
-          {date && <div className="font-light text-gray-500">{format(new Date(date), 'dd MMMM yyyy')}</div>}
+          {date && <div className={styles['header__date']}>{format(new Date(date), 'dd MMMM yyyy')}</div>}
         </div>
       )}
       {title && <Title>{title}</Title>}
