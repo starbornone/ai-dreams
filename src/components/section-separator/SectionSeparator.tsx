5a526e51e64838118f6933a661477abe5b693128
Sha <sha.kongbrooks@gmail.com>
2024-09-25 02:56:18 +1000

    Converted SectionSeparator to CSS

--- src/components/section-separator/SectionSeparator.tsx
+++ src/components/section-separator/SectionSeparator.tsx
@@ -1,5 +1,5 @@
+import styles from './SectionSeparator.module.css';
+
 export function SectionSeparator() {
-  return (
-    <hr className="h-1 mb-24 border-0 rounded-full mt-28 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500" />
-  );
+  return <hr className={styles['section-separator']} />;
 }
