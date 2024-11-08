191ed25ab5395e21f20e354c3af837650c86e853
Sha <sha.kongbrooks@gmail.com>
2024-11-09 06:09:53 +1100

    Remove author pic from post preview

--- src/features/posts/post-preview/PostPreview.tsx
+++ src/features/posts/post-preview/PostPreview.tsx
@@ -33,31 +33,7 @@ export function PostPreview({ post }: PostPreviewProps) {
           </Link>
         </h2>
         <div className="flex flex-col justify-between gap-4 text-sm text-gray-500 lg:flex-row lg:items-center">
-          <div className="flex flex-row items-center justify-between gap-2 lg:justify-start">
-            {/* Author */}
-            <Link aria-label="Read more about the author" className="flex items-center gap-2" href="/pages/about">
-              <Image
-                alt="Human"
-                aria-hidden="true"
-                className="h-8 w-auto rounded-full border-2 border-gray-900 bg-gray-800"
-                height={61}
-                src="https://res.cloudinary.com/starborn/image/upload/v1731091304/ai-dreams/profile/human.png"
-                width={36}
-              />
-            </Link>
-            {/* Date */}
-            {post.date && (
-              <time
-                className="flex items-center gap-2 lg:justify-end"
-                dateTime={post.date}
-                aria-label="Publication date"
-              >
-                <CalendarIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
-                {format(new Date(post.date), 'dd MMMM yyyy')}
-              </time>
-            )}
-          </div>
-          <div className="flex flex-row items-center justify-between gap-2 lg:justify-start">
+          <div className="flex flex-row items-center justify-between gap-4 lg:justify-start">
             {/* Category */}
             {post.category && (
               <div className="flex items-center gap-2" aria-label="Category">
@@ -76,6 +52,19 @@ export function PostPreview({ post }: PostPreviewProps) {
                 <TagIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
                 <span>{post.tags.map((tag) => tag).reduce((prev, curr) => [prev, ', ', curr] as any)}</span>
               </div>
+            )}
+          </div>
+          <div className="hidden lg:flex lg:flex-row lg:items-center lg:justify-between lg:gap-2">
+            {/* Date */}
+            {post.date && (
+              <time
+                className="flex items-center gap-2 lg:justify-end"
+                dateTime={post.date}
+                aria-label="Publication date"
+              >
+                <CalendarIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
+                {format(new Date(post.date), 'dd MMMM yyyy')}
+              </time>
             )}
           </div>
         </div>
