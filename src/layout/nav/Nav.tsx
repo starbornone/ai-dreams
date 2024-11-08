412d6ec8850a8e333a692f1bc8077533b8b6252e
Sha <sha.kongbrooks@gmail.com>
2024-11-09 05:03:31 +1100

    Improved category link clarity

--- src/layout/nav/Nav.tsx
+++ src/layout/nav/Nav.tsx
@@ -5,17 +5,22 @@ export async function Nav() {
   const { categories } = await handleCategoryCounts();
 
   return (
-    <div className="hidden lg:my-0 lg:block lg:max-w-2xl">
+    <nav aria-label="Primary Navigation" className="hidden lg:my-0 lg:block lg:max-w-2xl">
       <div
         className="neon-links flex flex-wrap justify-end space-x-12 text-xl tracking-wider text-gray-100"
         style={{ fontFamily: 'Neon' }}
       >
         {categories?.map((category: { slug: string; name: string }) => (
-          <Link href={`/categories/${category.slug}`} key={category.slug}>
+          <Link
+            aria-label={`Navigate to ${category.name}`}
+            href={`/categories/${category.slug}`}
+            key={category.slug}
+            title={category.name}
+          >
             {category.name}
           </Link>
         ))}
       </div>
-    </div>
+    </nav>
   );
 }
--- src/layout/nav/NavMobile.tsx
+++ src/layout/nav/NavMobile.tsx
@@ -5,18 +5,25 @@ export async function MobileNav() {
   const { categories } = await handleCategoryCounts();
 
   return (
-    <div className="m-16 block text-center lg:hidden">
-      <div className="sr-only">Topics:</div>
+    <nav aria-label="Mobile Navigation" className="m-16 block text-center lg:hidden">
+      <div className="sr-only" aria-hidden="true">
+        Topics:
+      </div>
       <div
         className="neon-links grid grid-cols-1 space-y-6 text-2xl tracking-wider text-gray-100"
         style={{ fontFamily: 'Neon' }}
       >
         {categories?.map((category: { slug: string; name: string }) => (
-          <Link href={`/categories/${category.slug}`} key={category.slug}>
+          <Link
+            aria-label={`Navigate to ${category.name}`}
+            href={`/categories/${category.slug}`}
+            key={category.slug}
+            title={category.name}
+          >
             {category.name}
           </Link>
         ))}
       </div>
-    </div>
+    </nav>
   );
 }
