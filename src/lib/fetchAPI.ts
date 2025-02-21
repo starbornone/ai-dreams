e9673a50b79ae330ae2cbcfecaa3a4935923df14
Sha <sha.kongbrooks@gmail.com>
2025-02-22 04:35:29 +1100

    Add delay to fetchAPI

--- src/lib/fetchAPI.ts
+++ src/lib/fetchAPI.ts
@@ -1,9 +1,25 @@
+let lastFetchTime = 0;
+const FETCH_DELAY = 3000;
+
+async function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export async function fetchAPI(query: string, { variables = {} } = {}) {
   const apiUrl = process.env.HYGRAPH_PROJECT_API;
 
   if (!apiUrl) {
     throw new Error('HYGRAPH_PROJECT_API is not defined');
   }
+
+  const now = Date.now();
+  const timeToWait = Math.max(0, FETCH_DELAY - (now - lastFetchTime));
+
+  if (timeToWait > 0) {
+    await delay(timeToWait);
+  }
+
+  lastFetchTime = Date.now();
 
   const res = await fetch(apiUrl, {
     method: 'POST',
