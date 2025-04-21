5eca321d8fd6909683a8bdc9261d8f1792aa0cd7
Sha <sha.kongbrooks@gmail.com>
2025-04-21 16:13:40 +1000

    Remove export of handlePostsCountByMonth for code cleanup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export * from './handleCategoryCounts';
 export * from './handleGetPage';
 export * from './handleGetPost';
 export * from './handleGetPostAndMorePost';
-export * from './handlePostsCountByMonth';
