ed1a07348f4b54ec10a61e4351860bc34695ca57
Sha <sha.kongbrooks@gmail.com>
2025-07-26 19:59:10 +1000

    Update Nav component: modify link spacing for better alignment and adjust max width for improved layout

--- src/layout/nav/Nav.tsx
+++ src/layout/nav/Nav.tsx
@@ -5,8 +5,8 @@ export async function Nav() {
   const { categories } = await handleCategoryCounts();
 
   return (
-    <nav aria-label="Primary Navigation" className="hidden lg:my-0 lg:block lg:max-w-xl">
-      <div className="neon-links flex flex-wrap justify-end space-x-8 font-neon text-xl tracking-wider text-gray-100">
+    <nav aria-label="Primary Navigation" className="hidden lg:my-0 lg:block lg:max-w-lg">
+      <div className="neon-links flex flex-wrap justify-end space-x-10 font-neon text-xl tracking-wider text-gray-100">
         {categories?.map((category: { slug: string; name: string }) => (
           <Link
             aria-label={`Navigate to ${category.name}`}
