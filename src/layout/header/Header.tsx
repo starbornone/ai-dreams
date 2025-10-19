776983b3aeb0100fc48670406b99b943661f75b4
Sha <sha.kongbrooks@gmail.com>
2025-10-19 21:34:19 +1100

    Better responsive header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -7,11 +7,11 @@ import './Header.css';
 export function Header() {
   return (
     <div className="header">
-      <div className="site-title">
-        <h1 className="header__title">
+      <div className="header__title site-title">
+        <h1 className="header__title-domain">
           <Link href="/">AI Dreams</Link>
         </h1>
-        <div className="header__subtitle">
+        <div className="header__title-subtitle">
           of a better
           <span className="sign header__sign">
             .<span className="fast-flicker">w</span>or
