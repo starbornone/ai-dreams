d18090250f0435c8e65f3040728335a8827938b3
Sha <sha.kongbrooks@gmail.com>
2025-12-28 03:43:30 +1100

    Remove 'Best of AI Dreams' page from MorePages component

--- src/features/more-pages/MorePages.tsx
+++ src/features/more-pages/MorePages.tsx
@@ -12,12 +12,6 @@ const pages = [
       'https://ap-southeast-2.graphassets.com/AnKXekidYRMi78mgukWzEz/resize=height:400,width:800/P7tLAcr7RyyAyyDd9Q7Y',
   },
   {
-    title: 'Best of AI Dreams',
-    href: 'pages/best-ai-dreams',
-    imageUrl:
-      'https://ap-southeast-2.graphassets.com/AnKXekidYRMi78mgukWzEz/resize=height:400,width:800/6znd7BOhRLS9vQcjPHQH',
-  },
-  {
     title: 'Book Recommendations',
     href: 'pages/book-recommendations-non-fiction-edition',
     imageUrl:
